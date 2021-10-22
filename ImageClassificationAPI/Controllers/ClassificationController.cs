0e94daaf5ca4132c897831d1b5a4b129f809d464
tomeraka@gmail.com <tomeraka@gmail.com>
2021-10-22 03:11:38 +0300

    Update controller with API backbone

---
+++ ImageClassificationAPI/Controllers/ClassificationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageClassificationAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClassificationController : ControllerBase
+    {
+        private readonly ILogger<ClassificationController> _logger;
+
+        public ClassificationController(ILogger<ClassificationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(200)]
+        public IActionResult ClassifyImage(IFormFile imageFile)
+        {
+            // validate image file, handle exceptions
+            // preprocess image
+            // create a tensor
+            // run model, get classifications
+            // return response
+            return Ok();
+        }
+    }
+}
